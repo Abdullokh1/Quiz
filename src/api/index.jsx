4a33bc991f40e678b86a03ce1c933cdb3e8b264e
Abdullokh <giyasovabdullo0@gmail.com>
2022-08-15 23:33:37 +0500

    initial commit

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getQuestions = async (count, navigate) => {
-  const res = await axios.get(`https://opentdb.com/api.php?amount=${count}&category=21`);
+  const res = await axios.get(`https://opentdb.com/api.php?amount=${count}&category=21&type=multiple`);
   navigate("/quiz", {
     state: {
       tests: res.data.results,
